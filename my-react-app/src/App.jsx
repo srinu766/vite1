5313b39512b7dab2223da49c6a8df927325cd514
srinu <sopparisrinu637@gmail.com>
2024-08-02 07:31:42 +0530

    first

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1 className="">Hello, Bootstrap!</h1>
+        <button className="btn btn-primary">Click me!</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
