6610688d24cc017bfb1875f889f3128580d808e1
srinu <sopparisrinu637@gmail.com>
2024-08-02 07:42:01 +0530

    first

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <div className="container">
         <h1 className="">Hello, Bootstrap!</h1>
-        <button className="btn btn-primary">Click me!</button>
+        <button className="btn btn-primary">Click me!kkk</button>
       </div>
     </div>
   );
